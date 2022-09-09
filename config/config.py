c81fafbc5ce68200c2c1113c7f11f4d79d874cab
spxjoaoregazzi <110836664+spxjoaoregazzi@users.noreply.github.com>
2022-09-09 16:05:44 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ date = str(lastbusyday(dt.date.today())) # Se vc ta vendo de D-1
 #date = str(dt.date.today()) # Se vc ta vendo de D0
 
 # BBG API
-username = 'spxrisk'
-password = 'risk@1234'
+username = 'coloque seu user'
+password = 'coloque sua senha'
 
 # OBS: Esse código está desatualizado e não considera feriados
